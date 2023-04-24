cf40a9875a0d5032fc5c8dbe6a049548bfb1f35d
JamesMathenge <mathengejames64@gmail.com>
2023-04-24 12:10:50 +0000

    update on main.h

--- main.h
+++ main.h
@@ -2,7 +2,10 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 void print_output(char output[], int *output_len);
+
 #endif
