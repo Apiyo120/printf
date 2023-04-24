30287f95cb687ebe12352edd1dee7f775be0330f
Apiyo120 <apiyodianah@gmail.com>
2023-04-24 15:34:06 -0700

    Printf_1

--- _printf_1.c
+++ _printf_1.c
@@ -1,94 +1,104 @@
 #include  "main.h"
 /**
- * _printf - function that produces output according to a format.
+ * _printf_1 - function that produces output according to a format.
  * @format: format string.
  * @... : variable arguments.
  * Return: the numbers of character printed.
 */
 
-int _printf(const char *format, ...)
+int _printf_1(const char *format, ...)
 {
+	va_list arguments;
+	int index = 0, count = 0;
+
 	if	(!format)
 		return (-1);
-	va_list arguments;
-
-	int index = 0, count = 0;
 
 	va_start(arguments, format);
 
-	while	 (format[index])
+	while (format[index])
 	{
-		if	(format[index] == '%')
+		if (format[index] == '%')
+		{
 			index++;
-
-		if	(format[index] == 'd' || format[index] == 'j')
-		{
-			int number = va_arg(arguments, int);
-			int is_negative = 0;
-
-			if	(number < 0)
-			{
-				is_negative = 1;
-			number = number;
-		}
-
-		int digits = 1, temp = number;
-
-		while (temp / 10)
-		{
-			digits++;
-			temp /= 10;
-		}
-
-		if	(is_negative)
-			count += _putchar('-');
-
-		for	(int i = digits - 1; i >= 0; i--)
-			count += _putchar((number / power(10, j)) % 10 + '0');
-		}
-		else if	(format[index] == 's')
-		{
-			char *str = va_arg(arguments, char *);
-
-			if	(!str)
-			{
-				str = "(null)';
-			}
-
-			while	(*str)
-			{
-				count += _putchar(*str);
-				str++;
-			}
+			count += handle_argument(format[index], arguments);
 		}
 		else
-		{
-			count += _putchar('%');
+
 			count += _putchar(format[index]);
-		}
-		index++;
+
 	}
 	va_end(arguments);
 	return (count);
 }
 
 /**
-*power - calculate base raised to the power of exponent.
-*@base: the base number.
-*@exponent: the exponent.
-*Return:the result of base raised to the power of exponent.
+ * handle_argument - function that handles each type of argument.
+ * @format: the format specifier.
+ * @arguments: the list of arguments.
+ * Return: the number of characters printed.
+*/
+int handle_argument(char format, va_list arguments)
+{
+	if	(format == 'd' || format == 'j')
+		return (handle_integer(va_arg(arguments, int)));
+	else
+		return (0);
+}
+
+/**
+ * handle_integer - function that handles integer arguments.
+ * @number: the integer of argument.
+ * Return: the number of characters printed.
+*/
+int handle_integer(int number)
+{
+	int count = 0, is_negative = 0, digits = 0;
+
+	if	(number < 0)
+	{
+		is_negative = 1;
+		number = -number;
+	}
+	digits = count_digits(number);
+
+	if	(is_negative)
+		count += _putchar('-');
+
+	while	(digits--)
+		count += _putchar((number / power(10, digits)) % 10 + '0');
+	return (count);
+}
+
+/**
+ * handle_string - function that handles string arguments.
+ * @str: the string argument.
+ * Return: the number of characters printed.
+*/
+
+int handle_string(char *str)
+{
+	int count = 0;
+
+	if	(!str)
+		str = "(null)";
+	while	(*str)
+		count += _putchar(*str++);
+	return (count);
+}
+
+/**
+ * power - calculate base raised to te powr of exponent.
+ * @base: the base number.
+ * @exponent: the exponent.
+ * Return: the result of base raised to the power of expinent.
 */
 
 int power(int base, int exponent)
 {
 	int result = 1;
-	int z = 0;
 
-	while	(z < exponent)
-	{
+	while	(exponent--)
 		result *= base;
-		z++;
-	}
 	return (result);
 }
-
--- main.h
+++ main.h
@@ -6,8 +6,13 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int _printf_1(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
-int power(int base, int exponent)
-
+int power(int base, int exponent);
+int handle_argument(char format, va_list arguments);
+int handle_integer(int number);
+int handle_string(char *str);
+int power(int base, int exponent);
+int count_digits(int number);
 #endif
